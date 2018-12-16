2e0f0f1fa190f626b07f0676de433d6617feb4da
Ryotaro Miyagwa <o_o_o_o_o1031@yahoo.co.jp>
2018-12-16 23:51:27 +0900

    walletのepicの作成

--- src/modules/Wallet.ts
+++ src/modules/Wallet.ts
@@ -1,7 +1,14 @@
 import { map, mergeMap } from "rxjs/operators";
-import { ajax } from "rxjs/ajax";
-import { ofType } from "redux-observable";
+import { ofType, Epic } from "redux-observable";
 import { SimpleWallet, Password, NetworkTypes, NEMLibrary } from "nem-library";
+import { Action } from "../types/Action";
+import { Wallet } from "../types/Wallet";
+
+// interface
+interface Payload {
+  name: string;
+  password: string;
+}
 
 // constants
 export const CREATE_WALLET = "CREATE_WALLET";
@@ -10,3 +17,26 @@ export const SET_WALLET = "SET_WALLET";
 // actions
 export const createWallet = () => ({ type: CREATE_WALLET });
 export const setWallet = () => ({ type: SET_WALLET });
+
+// epics
+export const createWalletEpic: Epic<Action<Payload>, any, Wallet> = (
+  action$,
+  state$
+) =>
+  action$.pipe(
+    ofType(CREATE_WALLET),
+    map(() => setWallet())
+  );
+
+// reducer
+export const weatherReducer = (
+  state: Wallet = {},
+  action: Action<Payload>
+): Wallet => {
+  switch (action.type) {
+    case SET_WALLET:
+      return state;
+    default:
+      return state;
+  }
+};
---
+++ src/types/Wallet.ts
@@ -0,0 +1,7 @@
+import { SimpleWallet } from "nem-library";
+
+export interface Wallet {
+  name?: string;
+  address?: string;
+  wallet?: string;
+}
